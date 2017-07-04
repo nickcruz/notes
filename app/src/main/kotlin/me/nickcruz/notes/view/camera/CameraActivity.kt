1aae736a1b7f9957aeb3465d182b5d66f735a814
Nick Cruz <ncruz@umich.edu>
2017-07-04 12:53:02 -0500

    Modified initial camera setup to use RxCamera2 bindings from library.
    Github link:
    https://github.com/FrangSierra/RxCamera2

--- app/src/main/kotlin/me/nickcruz/notes/view/camera/CameraActivity.kt
+++ app/src/main/kotlin/me/nickcruz/notes/view/camera/CameraActivity.kt
@@ -1,108 +1,122 @@
 package me.nickcruz.notes.view.camera
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
 import android.arch.lifecycle.LifecycleActivity
 import android.content.Context
 import android.content.Intent
-import android.graphics.SurfaceTexture
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.TextureView
 import kotlinx.android.synthetic.main.activity_camera.*
-import kotlinx.android.synthetic.main.content_camera.*
 import me.nickcruz.notes.R
 import me.nickcruz.notes.model.Note
-import android.graphics.ImageFormat
 import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.params.StreamConfigurationMap
 import android.hardware.camera2.CameraManager
-import android.media.ImageReader
-import android.util.Size
-import android.media.ImageReader.OnImageAvailableListener
+import android.hardware.camera2.CameraMetadata
 import android.os.Handler
-import android.view.Surface
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import durdinapps.rxcamera2.RxCameraManager
+import durdinapps.rxcamera2.wrappers.RxOpenCameraEvent
 
 
 /**
  * Created by Nick Cruz on 6/24/17
  */
-class CameraActivity : LifecycleActivity(), TextureView.SurfaceTextureListener {
+class CameraActivity : LifecycleActivity() {
 
     companion object {
         val EXTRA_NOTE = "note"
 
         fun getStartIntent(context: Context, note: Note) =
             Intent(context, CameraActivity::class.java).apply { putExtra(EXTRA_NOTE, note) }
+
+        private val REQUEST_CAMERA_PERMISSION = 1
     }
 
-    var previewSurfaceTexture: SurfaceTexture? = null
+    private lateinit var rxCameraManager: RxCameraManager
+    private lateinit var rearCameraId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         setActionBar(toolbar)
 
+        rxCameraManager = RxCameraManager(getSystemService(Context.CAMERA_SERVICE) as CameraManager)
 
-        // Step 1: Set a surface texture listener to know when the surface texture is available
-        previewTextureView.surfaceTextureListener = this
-    }
+        rearCameraId = getRearFacingCamera(rxCameraManager.cameraManager)
 
-    // Step 1: Surface Texture Listener implementation
-    override fun onSurfaceTextureSizeChanged(surfaceTexture: SurfaceTexture, width: Int, height: Int) {
-
-    }
-
-    override fun onSurfaceTextureUpdated(surfaceTexture: SurfaceTexture) {
-
-    }
-
-    override fun onSurfaceTextureDestroyed(surfaceTexture: SurfaceTexture): Boolean {
-        return true
-    }
-
-    override fun onSurfaceTextureAvailable(surfaceTexture: SurfaceTexture, width: Int, height: Int) {
-        previewSurfaceTexture = surfaceTexture
-
-        fetchCameraData()
-    }
-    // End Step 1
-
-    // Step 2: Fetch camera data
-    private var cameraManager: CameraManager? = null
-    private var cameraId: String? = null
-    private var cc: CameraCharacteristics? = null
-    private var streamConfigs: StreamConfigurationMap? = null
-    private var jpegSizes: Array<out Size>? = null
-
-    private fun fetchCameraData() {
-        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        cameraId = cameraManager?.cameraIdList?.get(0)
-        cc = cameraManager?.getCameraCharacteristics(cameraId)
-        streamConfigs = cc?.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-        jpegSizes = streamConfigs?.getOutputSizes(ImageFormat.JPEG)
-
-        setUpSurfacesForCapturingImages()
-    }
-    // End Step 2
-
-    // Step 3
-    private fun setUpSurfacesForCapturingImages() {
-        val width = jpegSizes?.get(0)?.width
-        val height = jpegSizes?.get(0)?.height
-        if (width != null && height != null) {
-            val jpegImageReader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1)
-            jpegImageReader.setOnImageAvailableListener({
-
-            }, Handler {
-
-            })
-
-            jpegImageReader
-
-
-        val previewSurface = Surface(previewSurfaceTexture)
-        val jpegCaptureSurface = jpegImageReader.surface
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestCameraPermission()
+            return
         }
 
+        openCamera(rxCameraManager, rearCameraId)
     }
-    // End Step 3
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            openCamera(rxCameraManager, rearCameraId)
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun openCamera(cameraManager: RxCameraManager, cameraId: String) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+            return
+        }
+        cameraManager.openCamera(cameraId, Handler())
+                .subscribe({ openCameraEvent ->
+                    when (openCameraEvent.eventType) {
+                        RxOpenCameraEvent.EventType.OPENED -> Log.v("CameraTest", "opened")
+                        RxOpenCameraEvent.EventType.DISCONNECTED -> Log.v("CameraTest", "disconnected")
+                        RxOpenCameraEvent.EventType.CLOSED -> Log.v("CameraTest", "closed")
+                        RxOpenCameraEvent.EventType.ERROR -> Log.v("CameraTest", "error")
+                        else -> Log.v("CameraTest", "neither of the normal 4")
+                    }
+                }, {
+                    Log.e("CameraTest", it.message)
+                })
+    }
+
+    /**
+     * Finds the rear-facing camera and returns the camera Id. For use with
+     * CameraManager.openCamera(cameraId, handler).
+     */
+    private fun getRearFacingCamera(cameraManager: CameraManager): String {
+        return cameraManager.cameraIdList
+                .map { it.to(cameraManager.getCameraCharacteristics(it)) }
+                .first { (_, characteristics) -> characteristics[CameraCharacteristics.LENS_FACING] == CameraMetadata.LENS_FACING_BACK }
+                .first
+    }
+
+    private fun requestCameraPermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            ConfirmationDialog().show(fragmentManager, null)
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+        }
+    }
+
+    class ConfirmationDialog : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+            val parent = parentFragment
+            return AlertDialog.Builder(activity)
+                    .setMessage(R.string.request_permission)
+                    .setPositiveButton(android.R.string.ok, { _, _ ->
+                        requestPermissions(arrayOf(Manifest.permission.CAMERA),
+                                REQUEST_CAMERA_PERMISSION)
+                    })
+                    .setNegativeButton(android.R.string.cancel,
+                            { _, _ ->
+                                val activity = parent.activity
+                                activity?.finish()
+                            })
+                    .create()
+        }
+    }
 }
